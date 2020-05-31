acf4223815f563c55e5eebd3660d1b3cd4ebf974
ryanmackintosh <ryan@esind.ca>
2020-05-30 21:59:07 -0700

    Commit

---
+++ CCC_2020_J2/CCC_2020_J2.c
@@ -0,0 +1,12 @@
+// Canadian Computing Competition
+// Year: 2020
+// Problem Junior #2 (J2)
+// Author: Ryan Mackintosh
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello, world!\n");
+	printf("Problem J2\n");
+	return 0;
+}
