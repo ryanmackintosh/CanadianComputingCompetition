4d69a880a96586c49a495b8d38fae2da6ac8fbbb
ryanmackintosh <ryan@esind.ca>
2020-05-30 21:49:18 -0700

    Add project files.

---
+++ CCC_2020_J1/CCC_2020_J1.c
@@ -0,0 +1,12 @@
+// Canadian Computing Competition
+// Year: 2020
+// Problem Junior #1 (J1)
+// Author: Ryan Mackintosh
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello, world!\n");
+
+	return 0;
+}
